cf3e06c04330e5a685ac3224e8b6a84a6e62604e
WONHEEJANG <jangwh93@gmail.com>
2021-12-16 09:30:34 +0900

    Initial Commit

---
+++ TMI-SwiftUI/TMI_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TMI_SwiftUIApp.swift
+//  TMI-SwiftUI
+//
+//  Created by Jason on 2021/12/16.
+//
+
+import SwiftUI
+
+@main
+struct TMI_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
